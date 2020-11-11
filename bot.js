711b9351ed3d79ea1794dc6b1afd60413d6d9ae0
kentlow2002 <kentlow2002@gmail.com>
2020-11-11 18:21:27 +0800

    lol

--- bot.js
+++ bot.js
@@ -6,10 +6,8 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
-       }
+client.on('guildMemberAdd', member => { 
+    member.guild.channels.get('channelID').send("Welcome");
 });
 
 // THIS  MUST  BE  THIS  WAY
