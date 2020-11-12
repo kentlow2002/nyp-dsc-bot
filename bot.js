7398812e24c3d2a95c3daf7870a0faa86c9f6c5f
kentlow2002 <kentlow2002@gmail.com>
2020-11-12 17:20:06 +0800

    catch stuff

--- bot.js
+++ bot.js
@@ -7,8 +7,12 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', (member) => {
-    member.guild.channels.cache.get("776009068610977792").send('new member');
-    member.roles.add(member.guild.roles.cache.get("776017709543850025"));
+    member.guild.channels.fetch("776009068610977792")
+        .then(ch => ch.send("new member"))
+        .catch(console.log("message went wrong!"));
+    member.guild.roles.fetch("776017709543850025")
+        .then(role => member.roles.add(role))
+        .catch(console.log("role went wrong!"));
     
 });
 
