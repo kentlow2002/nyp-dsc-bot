5d65d296d4c255919cbd6be441948ef80fc784d9
kentlow2002 <kentlow2002@gmail.com>
2020-11-16 16:42:53 +0800

    lol

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('guildMemberAdd', (member) => {
     .then(
         () => {
             doc.loadInfo()
-                .then(loadedDoc => console.log(loadedDoc.title))
+                .then(loadedDoc => console.log(loadedDoc))
                 .catch(error => console.log(`wow doc load fail heres ur shitty error\n${error}`));
         }
     )
