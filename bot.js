4ed6f3e66fbec82170d5853d646ee41f835f1da2
kentlow2002 <kentlow2002@gmail.com>
2020-11-18 15:55:17 +0800

    lol

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('guildMemberAdd', (member) => {
                                     sheet["_cells"].splice(0, 1);
                                     var cells = sheet["_cells"].map(cell => {
                                         console.log(cell[1].value);
-                                        if (cell[1].value != null) {
+                                        if (cell[1].value) {
                                             return cell[1].value;
                                         }
                                     });
